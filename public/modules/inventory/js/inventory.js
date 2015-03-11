7682d9cc35d63438b18f5c4a8230f5b7d8637eba
Daniel <danielhernandez16@hotmail.com>
2015-03-10 23:42:23 -0500

    Menu design options

--- public/modules/inventory/js/inventory.js
+++ public/modules/inventory/js/inventory.js
@@ -4,5 +4,22 @@
 
 (function() {
 	angular.module('indigo.inventory', [])
-		
+		.controller('TabsController', ['$scope', function ($scope) {
+			$scope.tab = 1;
+   	        $scope.show = false;
+
+			$scope.selectTab = function (tab) {
+				$scope.tab = tab;
+	   	        $scope.show = !$scope.show;
+
+			};
+
+			$scope.isActive = function (tab) {
+				return tab === $scope.tab;
+			}
+
+		    $scope.toggle = function () {
+		    	$scope.show = !$scope.show;
+		    };
+		}]); 
 })();
